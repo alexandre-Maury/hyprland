bc86422df2e8fe584b142a10943362291fd116cd
alexandre-Maury <alexandre_maury@outlook.com>
2024-10-08 20:46:27 +0200

    maj

--- test.sh
+++ test.sh
@@ -2,36 +2,55 @@
 
 # Variables
 DISK="/dev/sda"
-CFG_PART_EFI_SIZE="100"     # 100 Mo
-CFG_PART_SWAP_SIZE="4096"    # 4096 Mo
-CFG_PART_ROOT_SIZE="10240"   # 10 Go
-CFG_PART_HOME_SIZE="100%"    # Le reste
+CFG_PART_UEFI="y"            # "y" pour GPT/UEFI, "n" pour MBR/BIOS
+CFG_PART_EFI_SIZE="100M"      # Taille de la partition EFI en Mo
+CFG_PART_SWAP_SIZE="4096M"    # Taille de la partition SWAP en Mo
+CFG_PART_ROOT_SIZE="100%"     # Taille de la partition ROOT (le reste du disque)
 
 # Supprimer les partitions existantes
-sgdisk -Z ${DISK}
+echo "Suppression des partitions existantes sur ${DISK}..."
+parted ${DISK} mklabel gpt 2>/dev/null || parted ${DISK} mklabel msdos 2>/dev/null
 
-# Créer la table de partitions GPT
-sgdisk -og ${DISK}
+# Vérification du mode UEFI ou BIOS
+if [ "${CFG_PART_UEFI}" = "y" ]; then
+    echo "Création de partitions en mode GPT pour UEFI..."
 
-# Créer la partition EFI
-sgdisk -n 1:0:+${CFG_PART_EFI_SIZE}M -t 1:ef00 ${DISK}
+    # Créer une nouvelle table de partitions GPT
+    parted ${DISK} mklabel gpt
 
-# Créer la partition SWAP
-sgdisk -n 2:0:+${CFG_PART_SWAP_SIZE}M -t 2:8200 ${DISK}
+    # Créer les partitions
+    parted -a optimal ${DISK} mkpart primary fat32 1MiB ${CFG_PART_EFI_SIZE}
+    parted -a optimal ${DISK} set 1 esp on  # Définir la partition EFI
+    parted -a optimal ${DISK} mkpart primary linux-swap ${CFG_PART_EFI_SIZE} $((CFG_PART_EFI_SIZE + CFG_PART_SWAP_SIZE))
+    parted -a optimal ${DISK} mkpart primary ext4 ${CFG_PART_SWAP_SIZE} ${CFG_PART_ROOT_SIZE}
 
-# Créer la partition ROOT
-sgdisk -n 3:0:+${CFG_PART_ROOT_SIZE}M -t 3:8300 ${DISK}
+    # Formater les partitions
+    mkfs.vfat -F32 ${DISK}1  # Formater la partition EFI
+    mkswap ${DISK}2          # Formater la partition SWAP
+    mkfs.ext4 -L root-home ${DISK}3  # Formater la partition ROOT/HOME avec le label "root-home"
 
-# Créer la partition HOME (tout l'espace restant)
-sgdisk -n 4:0:0 -t 4:8302 ${DISK}
+    # Activer la partition SWAP
+    swapon ${DISK}2
 
-# Formater les partitions
-mkfs.vfat -F32 ${DISK}1  # Partition EFI
-mkswap ${DISK}2           # Partition SWAP
-mkfs.ext4 ${DISK}3        # Partition ROOT
-mkfs.ext4 ${DISK}4        # Partition HOME
+    echo "Partitionnement et formatage du disque en mode GPT (UEFI) terminé."
 
-# Activer la partition SWAP
-swapon ${DISK}2
+else
+    echo "Création de partitions en mode MBR pour BIOS..."
 
-echo "Partitionnement et formatage de ${DISK} terminé."
+    # Créer une nouvelle table de partitions MBR
+    parted ${DISK} mklabel msdos
+
+    # Créer les partitions
+    parted -a optimal ${DISK} mkpart primary linux-swap 1MiB ${CFG_PART_SWAP_SIZE}
+    parted -a optimal ${DISK} mkpart primary ext4 ${CFG_PART_SWAP_SIZE} ${CFG_PART_ROOT_SIZE}
+    parted ${DISK} set 2 boot on  # Définir la partition ROOT comme amorçable
+
+    # Formater les partitions
+    mkswap ${DISK}1                  # Formater la partition SWAP
+    mkfs.ext4 -L root-home ${DISK}2  # Formater la partition ROOT/HOME avec le label "root-home"
+
+    # Activer la partition SWAP
+    swapon ${DISK}1
+
+    echo "Partitionnement et formatage du disque en mode MBR (BIOS) terminé."
+fi
