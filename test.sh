d3219e90a2f046c189f6b3615bc286fb71fe7215
alexandre-Maury <alexandre_maury@outlook.com>
2024-10-10 00:01:03 +0200

    maj

--- test.sh
+++ test.sh
@@ -2,12 +2,12 @@
 
 # Variables
 BLOCK_DEVICE="$(lsblk -nd -o NAME,TYPE | awk '$2 == "disk" {print "/dev/" $1}')"
-PART_UEFI="y"
-PART_BOOTMBR_SIZE="256"    # Taille de la partition BOOT/MBR en MiB
-PART_BOOTEFI_SIZE="512"     # Taille de la partition BOOT/EFI en MiB
-PART_ROOT_SIZE="100"        # Taille de la partition pour / en GiB
-PART_HOME_SIZE="100"        # Taille de la partition pour /home en %
-FILE_SWAP_SIZE="2048"       # Taille du fichier SWAP en MiB
+PART_UEFI="y"                   # Choix entre UEFI (y) ou MBR (n)
+PART_BOOTMBR_SIZE="256"         # Taille de la partition BOOT/MBR en MiB
+PART_BOOTEFI_SIZE="512"          # Taille de la partition BOOT/EFI en MiB
+PART_ROOT_SIZE="100"             # Taille de la partition pour / en GiB
+PART_HOME_SIZE="100"             # Taille de la partition pour /home en %
+FILE_SWAP_SIZE="2048"            # Taille du fichier SWAP en MiB
 
 # Vérifier si le disque existe
 if [ ! -b "${BLOCK_DEVICE}" ]; then
@@ -34,38 +34,50 @@ else
 fi
 
 # Partitionnement
-if [ "${PART_UEFI}" == "y" ]; then
-    # Création des partitions pour le système EFI
+
+# $PART_ROOT_SIZE=$PART_ROOT_SIZE*1024 # converti valeur PART_ROOT_SIZE(GiB) en MiB
+
+    # # Configuration de l'étiquette du disque
+    # if [[ "$PART_UEFI" == "y" ]]; then
+
+    #     log_msg INFO "Début du partitionnement du disque ${BLOCK_DEVICE} en UEFI."
+    #     parted -a optimal ${BLOCK_DEVICE} --script mklabel gpt
+    #     parted -a optimal ${BLOCK_DEVICE} --script mkpart primary fat32 1MiB ${PART_BOOTEFI_SIZE}MiB # Partition EFI
+    #     parted -a optimal ${BLOCK_DEVICE} --script set 1 esp on 
+    #     parted -a optimal ${BLOCK_DEVICE} --script mkpart primary ext4 ${PART_BOOTEFI_SIZE}MiB $((PART_BOOTEFI_SIZE + PART_ROOT_SIZE))MiB # Partition \Racine
+    #     parted -a optimal ${BLOCK_DEVICE} --script mkpart primary ext4 $((PART_BOOTEFI_SIZE + PART_BOOT_SIZE))MiB ${PART_HOME_SIZE}%      # Partition \Home
+
+    # else
+
+    #     log_msg INFO "Début du partitionnement du disque ${BLOCK_DEVICE} en MBR."
+    #     parted -a optimal ${BLOCK_DEVICE} --script mklabel msdos
+    #     parted -a optimal ${BLOCK_DEVICE} --script mkpart primary ext4 1MiB ${PART_BOOTMBR_SIZE}MiB # Partition MBR
+    #     parted -a optimal ${BLOCK_DEVICE} --script set 1 boot on 
+    #     parted -a optimal ${BLOCK_DEVICE} --script mkpart primary ext4 ${PART_BOOTMBR_SIZE}MiB $((PART_BOOTMBR_SIZE + PART_ROOT_SIZE))MiB # Partition \Racine
+    #     parted -a optimal ${BLOCK_DEVICE} --script mkpart primary ext4 $((PART_BOOTMBR_SIZE + PART_ROOT_SIZE))MiB ${PART_HOME_SIZE}%      # Partition \Home
+    # fi
+
+
+
+
+
+# Conversion de PART_ROOT_SIZE de GiB en MiB
+PART_ROOT_SIZE_MB=$((PART_ROOT_SIZE * 1024))
+
+# Configuration de l'étiquette du disque
+if [[ "$PART_UEFI" == "y" ]]; then
+    log_msg INFO "Début du partitionnement du disque ${BLOCK_DEVICE} en UEFI."
     parted -a optimal "${BLOCK_DEVICE}" --script mklabel gpt
-    parted -a optimal "${BLOCK_DEVICE}" --script mkpart primary fat32 1MiB "${PART_BOOTEFI_SIZE}MiB"
-    parted -a optimal "${BLOCK_DEVICE}" --script set 1 esp on
-    parted -a optimal "${BLOCK_DEVICE}" --script mkpart primary ext4 "${PART_BOOTEFI_SIZE}MiB" "${PART_BOOTEFI_SIZE + PART_BOOTMBR_SIZE}MiB"
-    parted -a optimal "${BLOCK_DEVICE}" --script mkpart primary ext4 "${PART_BOOTEFI_SIZE + PART_BOOTMBR_SIZE}MiB" "${PART_BOOTEFI_SIZE + PART_BOOTMBR_SIZE + PART_ROOT_SIZE}GiB"
-    parted -a optimal "${BLOCK_DEVICE}" --script mkpart primary ext4 "${PART_BOOTEFI_SIZE + PART_BOOTMBR_SIZE + PART_ROOT_SIZE}GiB" 100%
-
-    # Formater les partitions
-    mkfs.fat -F32 "${BLOCK_DEVICE}1"  # Partition EFI
-    mkfs.ext4 "${BLOCK_DEVICE}2"       # Partition boot
-    mkfs.ext4 "${BLOCK_DEVICE}3"       # Partition racine
-    mkfs.ext4 "${BLOCK_DEVICE}4"       # Partition home
-
+    parted -a optimal "${BLOCK_DEVICE}" --script mkpart primary fat32 1MiB "${PART_BOOTEFI_SIZE}MiB" # Partition EFI
+    parted -a optimal "${BLOCK_DEVICE}" --script set 1 esp on 
+    parted -a optimal "${BLOCK_DEVICE}" --script mkpart primary ext4 "${PART_BOOTEFI_SIZE}MiB" "${PART_BOOTEFI_SIZE + PART_ROOT_SIZE_MB}MiB" # Partition Racine
+    parted -a optimal "${BLOCK_DEVICE}" --script mkpart primary ext4 "${PART_BOOTEFI_SIZE + PART_ROOT_SIZE_MB}MiB" "${PART_HOME_SIZE}%" # Partition Home
 else
-    # Création des partitions pour MBR
+    log_msg INFO "Début du partitionnement du disque ${BLOCK_DEVICE} en MBR."
     parted -a optimal "${BLOCK_DEVICE}" --script mklabel msdos
-    parted -a optimal "${BLOCK_DEVICE}" --script mkpart primary ext4 1MiB "${PART_BOOTMBR_SIZE}MiB"
-    parted -a optimal "${BLOCK_DEVICE}" --script mkpart primary ext4 "${PART_BOOTMBR_SIZE}MiB" "${PART_BOOTMBR_SIZE + PART_ROOT_SIZE}GiB"
-    parted -a optimal "${BLOCK_DEVICE}" --script mkpart primary ext4 "${PART_BOOTMBR_SIZE + PART_ROOT_SIZE}GiB" 100%
-
-    # Formater les partitions
-    mkfs.ext4 "${BLOCK_DEVICE}1"       # Partition boot
-    mkfs.ext4 "${BLOCK_DEVICE}2"       # Partition racine
-    mkfs.ext4 "${BLOCK_DEVICE}3"       # Partition home
+    parted -a optimal "${BLOCK_DEVICE}" --script mkpart primary ext4 1MiB "${PART_BOOTMBR_SIZE}MiB" # Partition MBR
+    parted -a optimal "${BLOCK_DEVICE}" --script set 1 boot on 
+    parted -a optimal "${BLOCK_DEVICE}" --script mkpart primary ext4 "${PART_BOOTMBR_SIZE}MiB" "$((PART_BOOTMBR_SIZE + PART_ROOT_SIZE_MB))MiB" # Partition Racine
+    parted -a optimal "${BLOCK_DEVICE}" --script mkpart primary ext4 "$((PART_BOOTMBR_SIZE + PART_ROOT_SIZE_MB))MiB" "${PART_HOME_SIZE}%" # Partition Home
 fi
 
-# Créer un fichier swap
-fallocate -l "${FILE_SWAP_SIZE}MiB" /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-
-echo "Partitionnement et formatage terminés avec succès."
