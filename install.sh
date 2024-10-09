1a5f1580da2d24984eaf777cab207b99fe9841c5
alexandre-Maury <alexandre_maury@outlook.com>
2024-10-09 16:13:20 +0200

    maj

--- install.sh
+++ install.sh
@@ -64,60 +64,71 @@ fi
 
 # Effacement des systèmes de fichiers existants
 if prompt_confirm "Effacer tout sur le périphérique cible ? (y/n)"; then
-  parted ${CFG_BLOCK_DEVICE} mklabel gpt 2>/dev/null || parted ${CFG_BLOCK_DEVICE} mklabel msdos 2>/dev/null
+
+    parted ${CFG_BLOCK_DEVICE} mklabel gpt 2>/dev/null || parted ${CFG_BLOCK_DEVICE} mklabel msdos 2>/dev/null
+
+    # Configuration de l'étiquette du disque
+    if [[ "$CFG_PART_UEFI" == "y" ]]; then
+        parted -a optimal ${CFG_BLOCK_DEVICE} --script mklabel gpt
+        parted -a optimal ${CFG_BLOCK_DEVICE} --script mkpart primary fat32 1MiB ${CFG_PART_EFI_SIZE}MiB
+        parted -a optimal ${CFG_BLOCK_DEVICE} --script set 1 esp on  # Définir la partition EFI
+    else
+        parted -a optimal ${CFG_BLOCK_DEVICE} --script mklabel msdos
+        parted -a optimal ${CFG_BLOCK_DEVICE} --script mkpart primary ext4 1MiB ${CFG_PART_EFI_SIZE}MiB # Partition Boot
+        parted -a optimal ${CFG_BLOCK_DEVICE} --script set 1 boot on # Définir la partition boot comme amorçable
+    fi
+
+
+    # Création des partitions
+    parted -a optimal ${CFG_BLOCK_DEVICE} --script mkpart primary ext4 $((CFG_PART_EFI_SIZE))MiB ${CFG_PART_ROOT_SIZE}  # Partition ROOT
+
+
+    # Configuration des systèmes de fichiers
+    if [[ "$CFG_PART_UEFI" == "y" ]]; then
+        mkfs.vfat -F32 ${CFG_BLOCK_DEVICE}1      
+    else
+        mkfs.ext4 -L boot ${CFG_BLOCK_DEVICE}1    
+    fi
+            
+    mkfs.ext4 -L root-home ${CFG_BLOCK_DEVICE}2
+
+    mkdir -p /mnt/gentoo
+    mount ${CFG_BLOCK_DEVICE}2 /mnt/gentoo
+
+    if [[ "$CFG_PART_UEFI" == "y" ]]; then
+        mkdir -p /mnt/gentoo/boot/EFI      
+        mount ${CFG_BLOCK_DEVICE}1 /mnt/gentoo/boot/EFI
+    else
+        mkdir -p /mnt/gentoo/boot
+        mount ${CFG_BLOCK_DEVICE}1 /mnt/gentoo/boot
+    fi
+
+    # Formater et créer le fichier swap
+    dd if=/dev/zero of=/mnt/gentoo/swap bs=1G count=${CFG_FILE_SWAP_SIZE}   # Créer un fichier swap de 4 Go
+    chmod 600 /mnt/gentoo/swap                             # Configurer les droits
+    mkswap /mnt/gentoo/swap                                # Formater le fichier swap
+    swapon /mnt/gentoo/swap    
+
+
+    log_msg INFO "Partitionnement et formatage du disque terminés avec succès."
+    parted -s "$CFG_BLOCK_DEVICE" print  # Affiche la table de partitions
 fi    
 
-# Configuration de l'étiquette du disque
-if [[ "$CFG_PART_UEFI" == "y" ]]; then
-    parted -a optimal ${CFG_BLOCK_DEVICE} --script mklabel gpt
-    parted -a optimal ${CFG_BLOCK_DEVICE} --script mkpart primary fat32 1MiB ${CFG_PART_EFI_SIZE}MiB
-    parted -a optimal ${CFG_BLOCK_DEVICE} --script set 1 esp on  # Définir la partition EFI
-else
-    parted -a optimal ${CFG_BLOCK_DEVICE} --script mklabel msdos
-    parted -a optimal ${CFG_BLOCK_DEVICE} --script mkpart primary ext4 1MiB ${CFG_PART_EFI_SIZE}MiB # Partition Boot
-    parted -a optimal ${CFG_BLOCK_DEVICE} --script set 1 boot on # Définir la partition boot comme amorçable
-fi
 
-
-# Création des partitions
-parted -a optimal ${CFG_BLOCK_DEVICE} --script mkpart primary ext4 $((CFG_PART_EFI_SIZE))MiB ${CFG_PART_ROOT_SIZE}  # Partition ROOT
-
-
-# Configuration des systèmes de fichiers
-if [[ "$CFG_PART_UEFI" == "y" ]]; then
-    mkfs.vfat -F32 ${CFG_BLOCK_DEVICE}1      
-else
-    mkfs.ext4 -L boot ${CFG_BLOCK_DEVICE}1    
-fi
-           
-mkfs.ext4 -L root-home ${CFG_BLOCK_DEVICE}2
-
-mkdir -p /mnt/gentoo
-mount ${CFG_BLOCK_DEVICE}2 /mnt/gentoo
-
-if [[ "$CFG_PART_UEFI" == "y" ]]; then
-    mkdir -p /mnt/gentoo/boot/EFI      
-    mount ${CFG_BLOCK_DEVICE}1 /mnt/gentoo/boot/EFI
-else
-    mkdir -p /mnt/gentoo/boot
-    mount ${CFG_BLOCK_DEVICE}1 /mnt/gentoo/boot
-fi
-
-# Formater et créer le fichier swap
-dd if=/dev/zero of=/mnt/gentoo/swap bs=1G count=${CFG_FILE_SWAP_SIZE}   # Créer un fichier swap de 4 Go
-chmod 600 /mnt/gentoo/swap                             # Configurer les droits
-mkswap /mnt/gentoo/swap                                # Formater le fichier swap
-swapon /mnt/gentoo/swap    
-
-
-log_msg INFO "Partitionnement et formatage du disque terminés avec succès."
-parted -s "$CFG_BLOCK_DEVICE" print  # Affiche la table de partitions
 
 # Copie et exécution de l'installation du stage3
-cp stage3.sh /mnt/gentoo/
-cp fonction.sh /mnt/gentoo/
-cp config.sh /mnt/gentoo/
-(cd /mnt/gentoo ; bash stage3.sh)
+if [ -d "/mnt/gentoo" ]; then
+    
+    cp stage3.sh /mnt/gentoo/
+    cp fonction.sh /mnt/gentoo/
+    cp config.sh /mnt/gentoo/
+    
+    # Exécution du script stage3.sh dans /mnt/gentoo
+    (cd /mnt/gentoo && bash stage3.sh)
+else
+    echo "Erreur : le répertoire /mnt/gentoo n'existe pas."
+    exit 1
+fi
 
 umount -R /mnt/gentoo  # Démontage récursif.
 
