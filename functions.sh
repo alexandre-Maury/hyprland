ac5cdf0765a2aa4deca671c306074807e530c4cb
alexandre-Maury <alexandre_maury@outlook.com>
2024-10-14 22:30:40 +0200

    maj

--- functions.sh
+++ functions.sh
@@ -26,7 +26,7 @@ prompt_confirm() {
     # Appliquer la couleur LIGHT_CYAN au prompt et réinitialiser après la saisie
     # read -r -p "${LIGHT_CYAN}${prompt}${RESET} " response
 
-    printf "%b[ %-7s ] %s: %s%b\n" ${LIGHT_CYAN} ${prompt} ${RESET}" "
+    printf "%b[ %-7s ] %s: %s%b\n" "${LIGHT_CYAN} ${prompt} ${RESET}"
     read response
 
     [[ "$response" =~ ^(y|Y|yes|YES)$ ]]
